4f70a5bdf0118b812bf7a8163716fbbe4f395b82
ettore-galli <ettore.galli@gmail.com>
2025-02-18 21:57:51 +0100

    start stop

--- led_ui/logic.py
+++ led_ui/logic.py
@@ -20,6 +20,7 @@ class SequenceStatus:
     active: bool
     position: int
     position_ticks_us: int
+    last_press_ticks_us: int
 
     def __init__(
         self,
@@ -27,11 +28,13 @@ class SequenceStatus:
         active: bool,
         position: int,
         position_ticks_us: int,
+        last_press_ticks_us: int,
     ) -> None:
         self.sequence = sequence
         self.active = active
         self.position = position
         self.position_ticks_us = position_ticks_us
+        self.last_press_ticks_us = last_press_ticks_us
 
     def start(self, position_ticks_us: int) -> None:
         self.active = True
@@ -59,40 +62,55 @@ class OneUI:
     def shoud_run(self) -> bool:
         return True
 
-    def notify_sequence(self, sequence: list[tuple[int, float]]) -> None:
-        for status, duration in sequence:
-            if status == LedStatus.ON:
-                self.config.led.on()
-            else:
-                self.config.led.off()
-            self.config.timer.sleep(seconds=duration)
-
     def ui_loop(self) -> None:
         sequence_status = SequenceStatus(
             sequence=[
                 (LedStatus.ON, 0.2),
                 (LedStatus.OFF, 0.2),
+                (LedStatus.ON, 0.2),
+                (LedStatus.OFF, 0.2),
+                (LedStatus.ON, 0.2),
+                (LedStatus.OFF, 0.6),
                 (LedStatus.ON, 0.6),
+                (LedStatus.OFF, 0.2),
+                (LedStatus.ON, 0.6),
+                (LedStatus.OFF, 0.2),
+                (LedStatus.ON, 0.6),
+                (LedStatus.OFF, 0.6),
+                (LedStatus.ON, 0.2),
                 (LedStatus.OFF, 0.2),
                 (LedStatus.ON, 0.2),
                 (LedStatus.OFF, 0.2),
+                (LedStatus.ON, 0.2),
+                (LedStatus.OFF, 0.6),
             ],
             active=False,
             position=0,
             position_ticks_us=0,
+            last_press_ticks_us=0,
         )
 
         while self.shoud_run():
 
             value = self.config.button.value()
 
+            current = self.config.timer.ticks_us()
+
             if value == ButtonStatus.PRESSED:
-                if not sequence_status.started():
-                    sequence_status.start(self.config.timer.ticks_us())
+                ticks_since_last = self.config.timer.ticks_diff(
+                    current, sequence_status.last_press_ticks_us
+                )
+                if ticks_since_last > 100000:
+                    print("TICKS PASSED", ticks_since_last)
+                    sequence_status.last_press_ticks_us = current
+                    if not sequence_status.started():
+                        sequence_status.start(current)
+                    else:
+                        sequence_status.stop()
 
             if sequence_status.started():
                 ticks = self.config.timer.ticks_diff(
-                    self.config.timer.ticks_us(), sequence_status.position_ticks_us
+                    current, sequence_status.position_ticks_us
                 )
 
                 status, duration_s = sequence_status.sequence[sequence_status.position]
@@ -100,7 +118,7 @@ class OneUI:
 
                 if ticks > duration_us:
                     sequence_status.position += 1
-                    sequence_status.position_ticks_us = self.config.timer.ticks_us()
+                    sequence_status.position_ticks_us = current
 
                     if sequence_status.position == len(sequence_status.sequence):
                         sequence_status.stop()
@@ -109,17 +127,3 @@ class OneUI:
                     self.config.led.on()
                 else:
                     self.config.led.off()
-
-                    # self.notify_sequence(
-                    #     [
-                    #         (LedStatus.ON, 0.2),
-                    #         (LedStatus.OFF, 0.2),
-                    #         (LedStatus.ON, 0.6),
-                    #         (LedStatus.OFF, 0.2),
-                    #         (LedStatus.ON, 0.2),
-                    #         (LedStatus.OFF, 0.2),
-                    #     ]
-                    # )
-            # else:
-            #     self.config.led.off()
-            self.config.timer.sleep(0.01)
